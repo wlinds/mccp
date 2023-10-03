92f937fb46de67d6f7bc93132979069e205836c2
William L <41084946+helveticae@users.noreply.github.com>
2023-10-03 10:44:19 +0200

    replace(" ", "_") in dir names

--- multicamcomposepro/create_warehouse_directories.py
+++ multicamcomposepro/create_warehouse_directories.py
@@ -14,6 +14,11 @@ class WarehouseBuilder:
         self.object_name = str()
         self.anomalies = [] # Used in camera.py
 
+    def clean_folder_name(self, folder_name: str):
+        cleaned_name = folder_name.replace(" ", "_")
+        return cleaned_name
+
+
     def create_directory(self, path: str):
         """
         Create a directory at the given path and add its name to the created_dirs list.
@@ -36,7 +41,7 @@ class WarehouseBuilder:
             object_name (str): The name of the object directory.
             anomalies (List[str]): A list of anomaly names for nested subdirectories.
         """
-        self.object_name = object_name
+        self.object_name = self.clean_folder_name(object_name)
         base_dir_path = os.path.join(os.getcwd(), "data_warehouse")
         dataset_dir_path = os.path.join(base_dir_path, "dataset")
         object_dir_path = os.path.join(dataset_dir_path, self.object_name)
@@ -55,6 +60,7 @@ class WarehouseBuilder:
             self.create_directory(sub_dir_path)
 
             for nested_sub_dir in nested_sub_dirs.get(sub_dir, []):
+                nested_sub_dir = self.clean_folder_name(nested_sub_dir)
                 nested_sub_dir_path = os.path.join(sub_dir_path, nested_sub_dir)
                 self.create_directory(nested_sub_dir_path)
 
