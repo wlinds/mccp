b84d2b45877490ca469d10e35bf3e67d1d49600e
Rufshod <alexander@11a.se>
2023-10-03 14:47:49 +0200

    added camera_config to utils.py, added it to main.

--- multicamcomposepro/main.py
+++ multicamcomposepro/main.py
@@ -1,13 +1,18 @@
-import cv2
 from datetime import datetime
-import platform
 from camera import CameraManager
-from utils import camera_text_overlay, Warehouse
+from utils import CameraIdentifier, camera_text_overlay, Warehouse
+import os
 
 # TODO Summarized:
 # Make modular grid of camera streams (i.e. not only a row, but columns as well)
 
 def main():
+    # Check if camera_config.json exists
+    if not os.path.exists("camera_config.json"):
+        print("camera_config.json not found. Running CameraIdentifier...")
+        camera_identifier = CameraIdentifier()
+        camera_identifier.identify_all_cameras()
+        camera_identifier.save_to_json()
     warehouse = Warehouse()
     warehouse.build(object_name="purple_duck", anomalies=["Albinism", "Melanism", "Polydactyly", "Missing Limbs"])
     camera_manager = CameraManager(warehouse)
--- multicamcomposepro/utils.py
+++ multicamcomposepro/utils.py
@@ -1,11 +1,13 @@
 import cv2
 import os
 from typing import List
-
-
+import json
+from platform import system
+import threading
 
 
 # Camera #
+
 
 def camera_text_overlay(frame, camera_name):
     font = cv2.FONT_HERSHEY_SIMPLEX
@@ -16,9 +18,8 @@ def camera_text_overlay(frame, camera_name):
     cv2.putText(frame, camera_name, position, font, font_scale, font_color, line_type)
 
 
-
-
 # File structures #
+
 
 class Warehouse:
     """
@@ -30,12 +31,11 @@ class Warehouse:
         self.created_sub_dirs = []
         self.created_nested_sub_dirs = []
         self.object_name = str()
-        self.anomalies = [] # Used in camera.py
+        self.anomalies = []  # Used in camera.py
 
     def clean_folder_name(self, folder_name: str):
         cleaned_name = folder_name.replace(" ", "_")
         return cleaned_name
-
 
     def create_directory(self, path: str):
         """
@@ -51,7 +51,9 @@ class Warehouse:
             except Exception as e:
                 print(f"An error occurred while creating {path}: {e}")
 
-    def build(self, object_name, anomalies: List[str] = ["Default1","Default2","Default3"]):
+    def build(
+        self, object_name, anomalies: List[str] = ["Default1", "Default2", "Default3"]
+    ):
         """
         Build the data warehouse directory structure based on the given object name and anomalies.
 
@@ -69,7 +71,9 @@ class Warehouse:
 
         self.create_directory(object_dir_path)
 
-        self.anomalies = anomalies # populates the anomalies list so that camera.py can use it
+        self.anomalies = (
+            anomalies  # populates the anomalies list so that camera.py can use it
+        )
         sub_dirs = ["train", "test"]
         nested_sub_dirs = {"train": ["good"], "test": ["good"] + anomalies}
 
@@ -89,7 +93,70 @@ class Warehouse:
         if self.created_sub_dirs:
             ret += f"Created the following subdirectories for object {self.object_name}: {', '.join(self.created_sub_dirs)}"
         if self.created_nested_sub_dirs:
-            ret += f"With nested subdirectories: {', '.join(self.created_nested_sub_dirs)}"
-        elif not any([self.created_dirs, self.created_sub_dirs, self.created_nested_sub_dirs]):
-            ret +=f"Directory {self.object_name} already exist! Nothing has been created."  
+            ret += (
+                f"With nested subdirectories: {', '.join(self.created_nested_sub_dirs)}"
+            )
+        elif not any(
+            [self.created_dirs, self.created_sub_dirs, self.created_nested_sub_dirs]
+        ):
+            ret += (
+                f"Directory {self.object_name} already exist! Nothing has been created."
+            )
         return ret
+
+
+#   camera order #
+
+
+class CameraIdentifier:
+    def __init__(self):
+        self.camera_mapping = {}
+        self.os_name = system()
+
+    def identify_camera(self, index, event):
+        cap = (
+            cv2.VideoCapture(index, cv2.CAP_DSHOW)
+            if self.os_name == "Windows"
+            else cv2.VideoCapture(index)
+        )
+        while not event.is_set():
+            ret, frame = cap.read()
+            cv2.imshow(f"Camera {index}", frame)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        cap.release()
+        cv2.destroyAllWindows()
+
+    def save_to_json(self, filename="camera_config.json"):
+        with open(filename, "w") as f:
+            json.dump({"Camera Order": self.camera_mapping}, f)
+
+    def get_camera_count(self):
+        max_tested = 10  # Change this if you have more than 10 cameras
+        for i in range(max_tested):
+            cap = (
+                cv2.VideoCapture(i, cv2.CAP_DSHOW)
+                if self.os_name == "Windows"
+                else cv2.VideoCapture(i)
+            )
+            if not cap.isOpened():
+                cap.release()
+                return i
+        return max_tested
+
+    def identify_all_cameras(self):
+        number_cameras = self.get_camera_count()
+        for i in range(number_cameras):
+            event = threading.Event()
+            thread = threading.Thread(target=self.identify_camera, args=(i, event))
+            thread.start()
+            identifier = input(f"Enter a unique identifier for camera at index {i}: ")
+            event.set()
+            thread.join()
+            if identifier.lower() == "skip":
+                if "skip" in self.camera_mapping:
+                    self.camera_mapping["skip"].append(i)
+                else:
+                    self.camera_mapping["skip"] = [i]
+            else:
+                self.camera_mapping[identifier] = i
