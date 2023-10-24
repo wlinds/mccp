6e71eb064b7c2ddb3e8a8f50cbbb656b2b7156e2
William L <41084946+helveticae@users.noreply.github.com>
2023-10-24 16:09:01 +0200

    new method names

--- src/multicamcomposepro/main.py
+++ src/multicamcomposepro/main.py
@@ -3,30 +3,24 @@ from camera import CameraManager
 from utils import CameraConfigurator, CameraIdentifier, Warehouse
 
 object_name = "aug_test2"
-
+anomalies = ["cracked screen", "discolored front"]
 
 def main():
-    # Existing code
-    camera_identifier = CameraIdentifier()
-    camera_identifier.camera_identifier()  # Check if camera_config.json exists
-
-    camera_configurator = CameraConfigurator()
-    camera_configurator.camera_configurator()
-
+    # Create a structured data warehouse
     warehouse = Warehouse()
-    warehouse.build(
-        object_name=object_name,
-        anomalies=["a n o m a ly"],
-    )
-    print(warehouse)
+    warehouse.build(object_name, anomalies)
+    
+    # Find all connected cameras
+    CameraIdentifier()
+    
+    # Configure all found cameras
+    CameraConfigurator().run()
 
     camera_manager = CameraManager(warehouse, 2, 3)
     camera_manager.run()
 
-    # Data Augmentation
     augmenter = DataAugmenter(object_name, temperature=10, logging_enabled=False)
-    augmenter.augment_images()  # You can pass selected_images if needed
-
+    augmenter.augment_images()  # Pass selected_images if needed
 
 if __name__ == "__main__":
     main()
